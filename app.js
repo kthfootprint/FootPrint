aa8bed271cdb91104a1a7e8db073020c90d157c3
adigladi <35582133+adigladi@users.noreply.github.com>
2019-04-25 16:32:36 +0200

    Travel-cards added with correct info

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 var apiKey = config.API_KEY;
+var icon = {
+    "WALKING": '<i class="fas fa-walking"></i>',
+    "BUS": '<i class="fas fa-bus"></i>',
+    "SUBWAY": '<i class="fas fa-subway"></i>',
+    "TRAIN": '<i class="fas fa-train"></i>'
+};
 
 document.addEventListener("DOMContentLoaded", function () {
     autoFill();
@@ -18,26 +24,28 @@ findRoute = () => {
                 console.log(result.routes[0]);
                 for (r in result.routes) {
                     var tType = [];
+                    var typeTime = [];
                     var tTime = 0;
                     for (t in result.routes[r].legs[0].steps) {
                         var step = result.routes[r].legs[0].steps[t];
                         var info = "";
                         if (step.transit_details) {
                             tTime += step.duration.value;
-                            info = step.transit_details.line.vehicle.type + " " + step.duration.text;
+                            tType.push(step.transit_details.line.vehicle.type);
+                            typeTime.push(step.duration.value);
                         } else {
-                            info = step.travel_mode + " " + step.duration.text;
+                            tType.push(step.travel_mode);
+                            typeTime.push(step.duration.value);
                         }
-
-                        tType.push(info);
                     }
                     var depart = result.routes[r].legs[0].departure_time.text;
+                    var arrive = result.routes[r].legs[0].arrival_time.text;
                     var totalTime = result.routes[r].legs[0].duration.text;
 
-                    var route = { departure: depart, duration: totalTime, type: tType, transitTime: tTime };
+                    var route = { departure: depart, arrival: arrive, duration: totalTime, type: tType, typeLength: typeTime, transitTime: tTime };
                     routeList.push(route);
                 }
-                routeList.sort(function(a, b) { 
+                routeList.sort(function (a, b) {
                     return a.transitTime - b.transitTime;
                 });
                 drawRoute(routeList);
@@ -50,10 +58,18 @@ findRoute = () => {
 }
 
 drawRoute = (list) => {
-    document.getElementById('routes').innerHTML = "<tr><th>Type</th><th>Departure</th><th>Travel Time</th></tr>";
+    document.getElementById('main').innerHTML = "";
     for (i in list) {
-        document.getElementById('routes').innerHTML +=
-        "<tr><td>" + list[i].type + "</td><td>" + list[i].departure + "</td><td>" + list[i].duration + "</td></tr>";
+        var travelSteps = "";
+        for (t in list[i].type) {
+            if (t > 0) {
+                travelSteps += '<i class="fas fa-chevron-right fa-xs"></i>' + icon[list[i].type[t]] + '<p><sub>' + Math.round(list[i].typeLength[t] / 60) + '</sub></p>';
+            } else {
+                travelSteps += icon[list[i].type[t]] + '<p><sub>' + Math.round(list[i].typeLength[t] / 60) + '</sub></p>';
+            }
+            
+        }
+        document.getElementById('main').innerHTML += '<article class="card"><div class="top"><div class="travel">' + travelSteps + '</div><div class="emission"><p>100 CO2</p></div><div class="time"><p>' + list[i].duration + '</p></div></div><div class="bottom"><p>' + list[i].departure + ' - ' + list[i].arrival + '</p></div></article>';
     }
 }
 
