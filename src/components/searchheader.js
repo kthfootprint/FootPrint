234e442db695ac1286a86622e519d9326d28bb86
Adrian Berger <adigladi@p37-n5.kthopen.kth.se>
2019-05-02 13:49:18 +0200

    More functions added

--- src/components/searchheader.js
+++ src/components/searchheader.js
@@ -4,7 +4,17 @@ import React from 'react';
 class SearchHeader extends React.Component {
 
   render() {
-     document.addEventListener("DOMContentLoaded", function () {
+    var icon = {
+      "WALKING": '<i className="fas fa-walking"></i>',
+      "BUS": '<i className="fas fa-bus"></i>',
+      "SUBWAY": '<i className="fas fa-subway"></i>',
+      "TRAIN": '<i className="fas fa-train"></i>',
+      "TRAM": '<i className="fas fa-bus-alt"></i>',
+      "FERRY": '<i className="fas fa-ship"></i>'
+    };
+    var orig = "";
+
+    document.addEventListener("DOMContentLoaded", function () {
       autoFill();
       document.getElementById('origin-input').onclick = function () {
         if (this.value === "My location") {
@@ -12,6 +22,22 @@ class SearchHeader extends React.Component {
         }
       };
     });
+
+    const drawRoute = (list) => {
+      document.getElementById('main').innerHTML = "";
+      for (var i in list) {
+        var travelSteps = "";
+        for (var t in list[i].type) {
+          if (t > 0) {
+            travelSteps += '<i className="fas fa-chevron-right fa-xs"></i>' + icon[list[i].type[t]] + '<p><sub>' + Math.round(list[i].typeLength[t] / 60) + '</sub></p>';
+          } else {
+            travelSteps += icon[list[i].type[t]] + '<p><sub>' + Math.round(list[i].typeLength[t] / 60) + '</sub></p>';
+          }
+
+        }
+        document.getElementById('main').innerHTML += '<article className="card"><div className="top"><div className="travel">' + travelSteps + '</div><div className="emission"><p>100 CO2</p></div><div className="time"><p>' + list[i].duration + '</p></div></div><div className="bottom"><p>' + list[i].departure + ' - ' + list[i].arrival + '</p></div></article>';
+      }
+    }
 
     const autoFill = () => {
       var originInput = document.getElementById('origin-input');
@@ -25,23 +51,40 @@ class SearchHeader extends React.Component {
       destinationAutocomplete.setFields(['place_id']);
     }
 
+    const getLocation = () => {
+      var originInput = document.getElementById('origin-input');
+      var destinationInput = document.getElementById('destination-input');
+      originInput.value = "My location";
+      destinationInput.value = ' ';
+      destinationInput.select();
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(setPosition);
+      } else {
+        document.getElementById('main').innerHTML = "Geolocation is not supported by this browser.";
+      }
+    }
+
+    const setPosition = (position) => {
+      orig = position.coords.latitude + ', ' + position.coords.longitude;
+    }
+
     return (
       <header>
         <nav id="searchField">
-          <label class="inp">
-            <input id="origin-input" class="controls" type="text" placeholder="&nbsp;" />
-            <span class="label">Start</span>
-            <span class="label" id="location"><i onClick="getLocation()" class="fas fa-crosshairs fa-lg"></i></span>
+          <label className="inp">
+            <input id="origin-input" className="controls" type="text" placeholder="&nbsp;" />
+            <span className="label">Start</span>
+            <span className="label" id="location"><i onClick={getLocation()} className="fas fa-crosshairs fa-lg"></i></span>
           </label>
 
-          <label class="inp">
+          <label className="inp">
             <input id="destination-input" type="text" placeholder="&nbsp;" />
-            <span class="label">Destination</span>
+            <span className="label">Destination</span>
           </label>
         </nav>
 
         <nav id="goBtn">
-          <button type="button" class="btn" onClick="findRoute()"><i class="fas fa-shoe-prints fa-2x"></i>Go!</button>
+          <button type="button" className="btn" /* onClick={findRoute()} */><i className="fas fa-shoe-prints fa-2x"></i>Go!</button>
         </nav>
       </header>
     );
