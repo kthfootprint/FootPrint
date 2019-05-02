648139d68798ee7a1c9d11f93b6ecf2b8a63cb69
EmilDickson <emil.dickson@gmail.com>
2019-05-02 15:26:19 +0200

    map component has a map and is now added to App.js. Installed google-maps-react. Run yarn install after subsequent pull requests.

--- src/components/routemap.js
+++ src/components/routemap.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import {Map, GoogleApiWrapper} from 'google-maps-react';
 
-class RouteMap extends Component {
+// https://www.npmjs.com/package/google-maps-react
+
+
+export class RouteMap extends Component {
     render() {
         return (
-            <div>Map</div>
+            <Map google={this.props.google} zoom={14}></Map>
         );
     }
 }
 
-export default RouteMap;
+export default GoogleApiWrapper({
+    apiKey: ('AIzaSyC8nThaZKU2HvY_tKlPrEIeZRtlHpHWOy0')
+})(RouteMap);
