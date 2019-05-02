0c5fa9152ecd6af940c2e74b750956d387d92569
EmilDickson <emildickson@n179-p14.eduroam.kth.se>
2019-05-02 14:57:29 +0200

    started working on map component

---
+++ src/components/routemap.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class RouteMap extends Component {
+    render() {
+        return (
+            <div>Map</div>
+        );
+    }
+}
+
+export default RouteMap;
