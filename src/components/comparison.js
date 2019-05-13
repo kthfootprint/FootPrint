21faf4472631792a87b3f868aa72b8ec7bb02886
Jonatan Lindström <jonatan3@kth.se>
2019-05-13 12:27:35 +0200

    Tooltip fix

--- src/components/comparison.js
+++ src/components/comparison.js
@@ -11,7 +11,7 @@ export class Comparison extends Component {
               <p>  {Math.round(this.props.comparableNumber)} g CO2</p>
             <FontAwesomeIcon icon={faQuestionCircle} data-tip data-for="tooltipparuuu"/>
             <ReactTooltip id='tooltipparuuu' place="left" type="dark" effect="solid">
-              <span>This would be the emissions of driving a <br/> modern diesel/petrol/hybrid car the same<br/> distance as your route.</span>
+              <span>This would be the emissions <br/> of driving a modern <br/> diesel/petrol/hybrid car the same <br/> distance as your route.</span>
             </ReactTooltip>
           </div>
         );
