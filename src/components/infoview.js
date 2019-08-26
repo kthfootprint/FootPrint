6359df61ee35101976e49d45b789bc5394dc00de
EmilDickson <emil.dickson@gmail.com>
2019-08-26 15:21:35 +0200

    feat: started on an info-view

---
+++ src/components/infoview.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const InfoView = () => {
+  const infoText = [
+    `This is a public transportation planning tool that calculates carbon emissions.
+    To search for a trip, simply type your starting point and desired destination above and click the "Go!" button.`,
+    `You will be presented with a number of options, along with their carbon emissions. As a comparison, the emission
+    information for the same route, if instead travelling by car, is presented as well. When you have found a suitable
+    route, press the "Choose route" button to see further details.`
+  ];
+
+  return (
+    <div className="infoView">
+      <h2>Welcome to FootPrint!</h2>
+      <p>{infoText[0]}</p>
+      <p>{infoText[1]}</p>
+    </div>
+  );
+};
+
+export default InfoView;
