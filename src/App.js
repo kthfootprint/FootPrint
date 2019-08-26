ba6e8fa4960403c5822aea74d711c347ffc6a83c
Jonatan Lindström <jonatan3@kth.se>
2019-08-26 15:18:31 +0200

    Added authorization-functions.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import { withAuthentication } from './components/Auth';
+
 import "./App.scss";
 import SearchHeader from "./components/searchheader.js";
+import LoginPage from "./components/loginpage.js";
 //import GoogleApiWrapper from './components/routemap.js';
 
-const App = () => <SearchHeader />;
+import * as ROUTES from './constants/routes';
 
-export default App;
+const App = () => (
+  <Router>
+
+    <Switch>
+      <Route exact path={ROUTES.LANDING} component={SearchHeader}/>
+      <Route exact path={ROUTES.LOGIN} component={LoginPage}/>
+      {/* <Route component={NoMatch} /> */}
+    </Switch>
+  </Router>
+);
+
+export default withAuthentication(App);
