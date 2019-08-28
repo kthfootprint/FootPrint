f3b48cf5d347737ef741643b8bfb89a91a3ccb62
Jonatan Lindström <jonatan3@kth.se>
2019-08-28 14:34:26 +0200

    Added firebase-keys

---
+++ src/components/Firebase/config.js
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: "AIzaSyDq0LacdYcsxWTmDpKp1LIaFb2RI70-2xg",
+  authDomain: "kthfootprint.firebaseapp.com",
+  databaseURL: "https://kthfootprint.firebaseio.com",
+  projectId: "kthfootprint",
+  storageBucket: "kthfootprint.appspot.com",
+  messagingSenderId: "1056144965980",
+  appId: "1:1056144965980:web:e717ec63efb8dbab"
+};
+
+export default config;
