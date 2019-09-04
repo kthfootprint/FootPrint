8e6dbeb91eb58ae6be314d334e18585cd141cfcf
EmilDickson <emil.dickson@gmail.com>
2019-09-04 11:19:36 +0200

    feature: adds an error message for when a user request does not give any search results

---
+++ src/components/searcherror.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SearchError = () => {
+  return (
+    <div className="infoView">
+      Sorry, but that search query did not yield any results. Please try
+      something else.
+    </div>
+  );
+};
+
+export default SearchError;
